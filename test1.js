184117c280a99556f1dad570883f88c5098c4530
skunnath <skunnath@box.com>
2020-10-23 11:48:55 -0700

    adding screenshot issue

--- test1.js
+++ test1.js
@@ -9,5 +9,6 @@ test('My first test', async t => {
         .click('#submit-button')
 
         // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!')
+        .takeScreenshot()
 });
